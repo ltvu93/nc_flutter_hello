a306e77eefa44991c24f30faa6c2ce01f6e727f5
Nhan Cao <nhancv92@gmail.com>
2018-03-21 10:56:51 +0700

    clean

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:nc_flutter_hello/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new FriendlychatApp());
+    await tester.pumpWidget(new DemoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
