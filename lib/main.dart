058973864e84b1b88f9f061f91f4585671fc7561
Nhan Cao <nhancv92@gmail.com>
2018-03-21 10:58:56 +0700

    Simple 1: https://medium.com/flutter-io/zero-to-one-with-flutter-43b13fd7b354

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -22,12 +22,25 @@ class DemoScreen extends StatefulWidget {
 
 class DemoScreenState extends State<DemoScreen> with TickerProviderStateMixin {
 
+  final random = new Random();
+  int dataSet;
+
+  void changeData() {
+    setState(() {
+      dataSet = random.nextInt(100);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("DemoApp")),
-      body: null,
+      body: new Center(
+        child: new Text('Data set: $dataSet'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.refresh),
+        onPressed: changeData,
+      ),
     );
   }
 
