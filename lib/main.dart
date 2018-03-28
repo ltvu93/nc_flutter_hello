9e9f51ab06bd873b35325d74c197c10074b3c605
Nhan Cao <nhancv92@gmail.com>
2018-03-29 00:28:26 +0700

    update icon

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      theme: new ThemeData(
+        primaryColor: new Color(0xffee613a),
+        accentColor: new Color(0xffe04d25),
+      ),
       home: new MainScreen(),
     );
   }
@@ -32,22 +36,47 @@ class MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('SHOP'),
-        centerTitle: true,
-        leading: new IconButton(
-            icon: new Image.asset('images/menu_icon.png'),
-            tooltip: 'Air it',
-            onPressed: null),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.search), onPressed: null),
-          new IconButton(
-              icon: new Icon(Icons.notifications_active), onPressed: null),
-        ],
-      ),
-      body: new Center(
-        child: new Text('Hello'),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text('SHOP'),
+          centerTitle: true,
+          leading: new IconButton(
+              icon: new Image.asset('images/menu_icon.png'),
+              tooltip: 'Air it',
+              onPressed: null),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Image.asset('images/search_icon.png'),
+                onPressed: null),
+            new IconButton(
+                icon: new Image.asset('images/notify_icon.png'),
+                onPressed: null),
+          ],
+        ),
+        body: new ListView(
+          children: <Widget>[searchWidget, requestWidget, adWidget, productFilter],
+        ));
   }
+
+  Widget searchWidget = new Container(
+    height: 61.75,
+    color: const Color(0xffee613a),
+    child: new Row(),
+  );
+
+  Widget requestWidget = new Container(
+    height: 249.7,
+    color: const Color(0xffe04d25),
+    child: new Row(),
+  );
+
+  Widget adWidget = new Container(
+    child:  new Image.asset('images/banner.png'),
+  );
+
+  Widget productFilter = new Container(
+    height: 298.7,
+    padding: const EdgeInsets.only(top: 28.0),
+    color: const Color(0xffe5e5e5),
+    child: new Row(),
+  );
 }
